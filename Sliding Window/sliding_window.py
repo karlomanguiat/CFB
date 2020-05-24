5b3f042148fd34b8bf6f778813b636cb152b8657
KythTerrobias <37520931+KythTerrobias@users.noreply.github.com>
2020-05-24 12:06:38 +0800

    Modified header row output in sliding_window.py
    Sliding window columns now have unique headers and the labels column now has "Labels" as header.

--- Sliding Window/sliding_window.py
+++ Sliding Window/sliding_window.py
@@ -1,11 +1,11 @@
 import csv
 import math
 
-win_size = 5                                                        # Specify the size of the sliding window
+win_size = 3                                                        # Specify the size of the sliding window
 
-f = open('sample_data.csv', 'r')                                    # name of the file to be processed
+f = open('subset2.csv', 'r')                                    # name of the file to be processed
 re = csv.reader(f, quoting=csv.QUOTE_NONE)                          
-fo = open('sample_out.csv', 'w')                                    # name of the output file
+fo = open('subset2_w3.csv', 'w')                                    # name of the output file
 wr = csv.writer(fo, quoting=csv.QUOTE_NONE, lineterminator='\n')
 
 window = []
@@ -14,8 +14,12 @@ ctr = 0
 for row in re:
     feature_num = len(row) - 1                                      # reads how many cells per row excluding the labels column
     if(ctr == 0):                                                   # repeats the column header
+        header = row
         for i in range(0, win_size):
+            if(i != 0):
+                row = [cell + "_" + str(i) for cell in header]
             window = window + row[:-1]
+        window = window + ["Labels"]
         wr.writerow(window)
         window = []
         ctr+=1
@@ -34,5 +38,5 @@ for row in re:
 
 window.append(labels[math.ceil(win_size/2)-1])
 wr.writerow(window)
-f.close()                                                           # close
+f.close()
 fo.close()
